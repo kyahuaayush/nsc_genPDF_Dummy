e22835567934fa93f1776fe19c9ef76eabbbbbc4
Ayush Kumar <59443366+kyahuaayush@users.noreply.github.com>
2021-07-07 17:35:58 +0530

    Report and Certificate Update in index.js
    Report and Certificate is done and uploaded

--- index.js
+++ index.js
@@ -10,25 +10,26 @@ fs.writeFileSync('content.log', 'Hey there!');
 // sendWADoc(917982454237, '123123');
 
 
-app.get('/12/:id', async function(req, res) {
+app.get('/certificate/:id', async function(req, res) {
 
-  var docId = req.params.id;
+  var Id = req.params.id;
+  var docId = Id.substring(0,13);
   console.log(docId);
 
-  var docPDFName = docId;
+  var docPDFName = "Certi_"+docId+".pdf";
 
   //Document Type
     let options = {
         format: 'A4',
         path: docPDFName,
         landscape: true,
-        scale: .99
+        scale: .99,
     };
 
     //HTML FIle to Convert to pdf
     let file = [{
-        url: "http://getpdf.nutom.in/testReport.php?name=Ayush%20Kumar",
-        name: 'ReportName.pdf'
+        url: "http://nsc.nutom.in/3Cs/?uid="+docId,
+        name: 'Certificate.pdf'
     }];
 
     //Converting HTML to PDF
@@ -71,6 +72,64 @@ app.get('/', function(req, res) {
 })
 
 
+app.get('/report/:id', async function(req, res) {
+
+  var Id = req.params.id;
+  var docId = Id.substring(0,13);
+  // console.log(docId);
+
+  var docPDFName = "Report_"+docId+".pdf";
+
+  //Document Type
+    let options = {
+        format: 'A4',
+        path: docPDFName,
+        landscape: false,
+        scale: .98,
+    };
+
+    //HTML FIle to Convert to pdf
+    let file = [{
+        url: "http://nsc.nutom.in/3Cs/report.php/?uid="+docId,
+        name: 'Report.pdf'
+    }];
+
+    //Converting HTML to PDF
+    downloadPdf();
+
+    //waiting for 5 sec to complete the conversion and  download
+    setTimeout(await function() {
+
+      res.download(docPDFName);
+      
+    }, 5000);
+
+
+        //after 5 sec of download, deleting the file coverted
+    setTimeout(await function() {
+
+            // res.redirect("/");
+            var fileDelPath = docPDFName;
+            fs.unlinkSync(fileDelPath);
+
+        }, 10000);
+
+
+
+
+    async function downloadPdf() {
+        const htmlDoc = await html_to_pdf.generatePdfs(file, options).then(output => {
+            console.log("PDF Buffer:-", output);
+        });
+    }
+
+
+
+
+})
+
+
+
 
 
 app.listen();
@@ -81,33 +140,3 @@ app.listen(port, () => {
 })
 
 
-
-//send file function
-
-function sendWADoc(phoneNo, id){
-
-
-const FormData = require('form-data');
-const fetch = require('node-fetch');
-const formData = new FormData();
-
-formData.append('file', fs.createReadStream('123123.pdf'));
-
-const url = "https://live-server-1867.wati.io/api/v1/sendSessionFile/"+phoneNo+"?caption=NSC 3C's Certificate";
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzU2M2I1MS1kOWQxLTRjZGYtODNjNC1mODBiNTc4MDM2OGIiLCJ1bmlxdWVfbmFtZSI6ImNvbm5lY3QubnV0b21AZ21haWwuY29tIiwibmFtZWlkIjoiY29ubmVjdC5udXRvbUBnbWFpbC5jb20iLCJlbWFpbCI6ImNvbm5lY3QubnV0b21AZ21haWwuY29tIiwiYXV0aF90aW1lIjoiMDYvMDQvMjAyMSAxNjoxNDoxMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.HUvT23dtsOdrhmRdzoiUIYc8FYmEEJufQbEXYPxn7Gw'
-  }
-};
-
-options.body = formData;
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-
-
-}
