1c829200ad3959c6a59a291266e39d1ed304b224
Ayush Kumar <59443366+kyahuaayush@users.noreply.github.com>
2021-07-08 13:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -140,3 +140,33 @@ app.listen(port, () => {
 })
 
 
+
+//send file function
+
+function sendWADoc(phoneNo, id){
+
+
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+const formData = new FormData();
+
+formData.append('file', fs.createReadStream('123123.pdf'));
+
+const url = "https://live-server-1867.wati.io/api/v1/sendSessionFile/"+phoneNo+"?caption=NSC 3C's Certificate";
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzU2M2I1MS1kOWQxLTRjZGYtODNjNC1mODBiNTc4MDM2OGIiLCJ1bmlxdWVfbmFtZSI6ImNvbm5lY3QubnV0b21AZ21haWwuY29tIiwibmFtZWlkIjoiY29ubmVjdC5udXRvbUBnbWFpbC5jb20iLCJlbWFpbCI6ImNvbm5lY3QubnV0b21AZ21haWwuY29tIiwiYXV0aF90aW1lIjoiMDYvMDQvMjAyMSAxNjoxNDoxMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.HUvT23dtsOdrhmRdzoiUIYc8FYmEEJufQbEXYPxn7Gw'
+  }
+};
+
+options.body = formData;
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
+
+
+}
